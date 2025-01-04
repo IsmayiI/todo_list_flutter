ecdd64c688b74e0159b5dc17ddc90c472a72bd5e
Ismayil <ismayilov_ismayil_97@mail.ru>
2025-01-04 20:30:28 +0400

    first commit

---
+++ lib/widgets/app/my_app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/widgets/groups/groups_widget.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme:
+            AppBarTheme(backgroundColor: Colors.blue, centerTitle: true),
+        useMaterial3: false,
+      ),
+      routes: {
+        '/groups': (context) => const GroupsWidget(),
+      },
+      initialRoute: '/groups',
+    );
+  }
+}
---
+++ lib/widgets/groups/groups_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class GroupsWidget extends StatelessWidget {
+  const GroupsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Группы'),
+      ),
+      body: const _GroupListWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _GroupListWidget extends StatefulWidget {
+  const _GroupListWidget({super.key});
+
+  @override
+  State<_GroupListWidget> createState() => _GroupListWidgetState();
+}
+
+class _GroupListWidgetState extends State<_GroupListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        itemCount: 20,
+        itemBuilder: (BuildContext context, int index) {
+          return _GroupListRowWidget(
+            index: index,
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider(
+            height: 1,
+          );
+        });
+  }
+}
+
+class _GroupListRowWidget extends StatelessWidget {
+  final int index;
+  const _GroupListRowWidget({required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      endActionPane: const ActionPane(
+        motion: BehindMotion(),
+        children: [
+          SlidableAction(
+            onPressed: null,
+            backgroundColor: Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          )
+        ],
+      ),
+      child: const ListTile(
+        title: Text('Группа'),
+        trailing: Icon(Icons.chevron_right),
+        onTap: null,
+      ),
+    );
+  }
+}
